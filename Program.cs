403566f0701000212335c841006822123b49b4d1
valitakacs <vali.flaviu.takacs@gmail.com>
2020-10-19 23:07:53 +0300

    Tema1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Moore_s_Law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insereaza un numar: ");
+
+            int n = int.Parse(Console.ReadLine());
+            double year = Math.Log(n, 2) * 1.5;
+
+            Console.WriteLine(year);
+
+        }
+    }
+}
